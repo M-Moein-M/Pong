6a640ea1a1458a266f05323cb738a6a99fed36f9
Moein <mohammadmoein.moradi@gamil.com>
2020-08-12 13:08:04 +0430

    added a bit of documentation

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ function gameInit() {
 
     let gameBall = document.getElementById('ball');
     gameBall.style.left = '300px';
-    gameBall.style.top = '50px'
+    gameBall.style.top = '200px'
 
-    let gameBallVx = 2;
-    let gameBallVy = 2;
+    let gameBallVx = 2; // indicates the velocity of the ball in x axis
+    let gameBallVy = 2; // indicates the velocity of the ball in y axis
 
-    let gameInterval = setInterval(movement, 5);
+    let gameInterval = setInterval(movement, 5);  // this moves the ball every interval
 
-    function movement() {
+    function movement() {  // moves the ball based on vx and vy and also checks for wall collision to change the vx or vy accordingly
         let current_x = getBallX();
         let current_y = getBallY();
 
@@ -28,11 +28,11 @@ function gameInit() {
     }
 
     function getBallX() {
-        return Number(gameBall.style.left.replace('px', ''));
+        return Number(gameBall.style.left.replace('px', ''));  // returns left of the ball image this will be considered as x
     }
 
     function getBallY() {
-        return Number(gameBall.style.top.replace('px', ''));
+        return Number(gameBall.style.top.replace('px', '')); // returns top of the ball image this will be considered as y
     }
 }
 
