f11b439c9c69ce28bd36429d242445191194e562
Moein <mohammadmoein.moradi@gamil.com>
2020-08-12 14:47:24 +0430

    User can now move the sticks to play. for the left side player(player1) keys are W and S. And for the right player(player2) keys are arrowup and arrowdown.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function gameInit() {
     let gameBallVx = 2; // indicates the velocity of the ball in x axis
     let gameBallVy = 2; // indicates the velocity of the ball in y axis
 
-    let gameInterval = setInterval(movement, 5);  // this moves the ball every interval
+    let gameInterval = setInterval(movement, 50);  // this moves the ball every interval
 
     function movement() {  // moves the ball based on vx and vy and also checks for wall collision to change the vx or vy accordingly
         let current_x = getBallX();
@@ -34,7 +34,57 @@ function gameInit() {
     function getBallY() {
         return Number(gameBall.style.top.replace('px', '')); // returns top of the ball image this will be considered as y
     }
+
+
+    // moving the player1 and player2's stick
+    let player1StickMoveTop;
+    let player1StickMoveDown;
+    let player2StickMoveTop;
+    let player2StickMoveDown;
+
+    let player1Stick = document.getElementById('player1-stick');
+    let player2Stick = document.getElementById('player2-stick');
+    let deltaY = 32; // this indicates amount of movement for each stick in every move
+    function moveElementUp(element) {
+        let top = Number(element.style.top.replace('px', ''));
+        let newPositionTop = top - deltaY;
+        if (newPositionTop < 0) // we dont want the sticks to move up more that the game div
+            return;
+        element.style.top = (newPositionTop).toString() + 'px';
+    }
+
+    function moveElementDown(element) {
+        let top = Number(element.style.top.replace('px', ''));
+        let newPositionTop = top + deltaY;
+        if (newPositionTop + 80 > gameDivHeight) // 80 is the height of the sticks
+            return;
+        element.style.top = (newPositionTop).toString() + 'px';
+    }
+
+    function moveStick(event) {
+        // player 1 KeyW KeyS
+        // player 2 ArrowUp ArrowDown
+        switch (event.code) {
+            case 'KeyW':
+                moveElementUp(player1Stick);
+                break;
+            case 'KeyS':
+                moveElementDown(player1Stick);
+                break;
+            case 'ArrowUp':
+                moveElementUp(player2Stick);
+                break;
+            case 'ArrowDown':
+                moveElementDown(player2Stick);
+                break;
+        }
+        console.log(event.code);
+    }
+
+
+    window.addEventListener('keyup', moveStick);
 }
+
 
 window.addEventListener('load', function () {
     console.log('game started')
